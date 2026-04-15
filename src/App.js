74e0a42b90413ff1c7dcd4db683466d89ae8a1b1
EshanyaSeetharam <eshanyabhat.2000@gmail.com>
2026-04-15 15:09:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -8,13 +8,17 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import GalleryPage from './pages/GalleryPage';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <NavBar />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
